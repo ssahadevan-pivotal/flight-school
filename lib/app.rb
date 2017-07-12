f84f0a13b5459e680fabdaba2a935593c7643c3b
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-07-12 09:06:13 -0500

    Replace LAX by OMA

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "LAX", "STL", "MAA"] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA"] }
   end
 
   get "/airports/:code" do
