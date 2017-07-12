4807f11630bc45fb0b36ae5f7f98411c979e2173
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-07-12 09:58:28 -0500

    Added ORD

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA", "ATL"] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA", "ORD"] }
   end
 
   get "/airports/:code" do
