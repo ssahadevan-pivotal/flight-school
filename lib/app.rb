096b6eb76720a195a8ad4fcae9429c69ae4a6b7b
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-07-12 21:54:57 -0500

    Removed MCI

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA", "MCI"] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA" ] }
   end
 
   get "/airports/:code" do
