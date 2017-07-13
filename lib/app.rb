2fd808159d842741fc193eed0990c2b30dd23ec6
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-07-12 22:22:32 -0500

    Changed MAA to MCI

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MAA" ] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MCI" ] }
   end
 
   get "/airports/:code" do
