811074cd0b247e439122bab9bc6ef690338c2a62
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-08-29 15:29:03 -0500

    STL to MAA
    STL replaced by MAA

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "STL", "MCI" ] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "MAA", "MCI" ] }
   end
 
   get "/airports/:code" do
