d83762e04ede0ebfe63a48b1e76817b2f9470426
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-09-14 16:28:19 -0500

    Three changes
    OHR-ORD, FWA-STL, OAK-BOM

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "MAA", "LAX" ] }
+    erb :index, locals: { airports: ["JFK", "SFO", "ORD", "STL", "BOM", "SEA", "OMA", "MAA", "LAX" ] }
   end
 
   get "/airports/:code" do
