3caabfa4cedfe68b0fb6333d771b6b7e0b4be838
Sharath Sahadevan <ssahadevan@pivotal.io>
2017-09-14 15:55:14 -0500

    Changed to LAX

--- lib/app.rb
+++ lib/app.rb
@@ -5,7 +5,7 @@ require_relative "radar"
 
 class FlightSchool < Sinatra::Base
   get "/" do
-    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "MAA", "STL" ] }
+    erb :index, locals: { airports: ["JFK", "SFO", "OHR", "FWA", "OAK", "SEA", "OMA", "MAA", "LAX" ] }
   end
 
   get "/airports/:code" do
